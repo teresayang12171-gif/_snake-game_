eefa87809204996cc82eccde350c75fb4d9542f7
楊琇晴 <teresayang12171@gmail.com>
2026-05-12 16:41:36 +0800

    修改

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig({
+  base: '/_snake-game_/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
